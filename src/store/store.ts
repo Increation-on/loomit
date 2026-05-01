7f72f4295cbd5b908099efc8cb937ce54b18161c
Increation-on <maxim.d132213@gmail.com>
2026-05-01 09:49:38 +0300

    resolve conflict

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,36 @@
 // src/store/store.ts
 import { configureStore } from '@reduxjs/toolkit';
+<<<<<<< Updated upstream
+=======
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { attemptsApi } from './api/attemptsApi';
+import { quizApi } from './api/quizApi';
+import quizReducer from './slices/quizSlice';
+>>>>>>> Stashed changes
 
 // Временный пустой редюсер
 const emptyReducer = (state = {}) => state;
 
 export const store = configureStore({
   reducer: {
+<<<<<<< Updated upstream
     _empty: emptyReducer,
   },
+=======
+    quiz: persistedQuizReducer,
+    [attemptsApi.reducerPath]: attemptsApi.reducer,
+    [quizApi.reducerPath]: quizApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    })
+      .concat(attemptsApi.middleware)
+      .concat(quizApi.middleware),
+>>>>>>> Stashed changes
 });
 
 export type RootState = ReturnType<typeof store.getState>;

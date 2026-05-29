e7df9f83379a25b5c9a6ea3415be181374ed4799
Increation-on <maxim.d132213@gmail.com>
2026-05-29 12:56:48 +0300

    update serwist config with proper sw.ts and webpack build

--- app/sw.ts
+++ app/sw.ts
@@ -1,3 +1,17 @@
-import { defaultCache } from '@serwist/turbopack/worker';
+import { defaultCache } from "@serwist/next/worker";
+import type { PrecacheEntry } from "serwist";
+import { Serwist } from "serwist";
 
-export { defaultCache };
+declare const self: ServiceWorkerGlobalScopeEventMap & {
+  __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
+};
+
+const serwist = new Serwist({
+  precacheEntries: self.__SW_MANIFEST,
+  skipWaiting: true,
+  clientsClaim: true,
+  navigationPreload: true,
+  runtimeCaching: defaultCache,
+});
+
+serwist.addEventListeners();
